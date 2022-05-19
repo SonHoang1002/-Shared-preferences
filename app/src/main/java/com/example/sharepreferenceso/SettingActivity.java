0f819906730c72476af6e6b44417af7257532d62
Admin <hoangtrungson07012001@gmail.com>
2022-05-19 12:49:25 +0700

    Share Preference

---
+++ app/src/main/java/com/example/sharepreferenceso/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.sharepreferenceso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
